75e980c558b906797ba8176006536c28bebee57c
Guilherme Ferrini <guilhermemferrini@gmail.com>
2020-08-05 19:19:30 -0300

    Add server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,1 +1,17 @@
-console.log('helloooo');
+import express from 'express';
+
+const app = express();
+
+const PORT = 3333;
+
+app.get('/users', (request, response) => {
+  const users = [
+    { name: 'gui', age: 31 },
+    { name: 'ze', age: 30 }
+  ]
+
+  return response.json(users); //retorna um json;
+  // return response.send('foi') //retorna um texto;
+})
+
+app.listen(PORT)
